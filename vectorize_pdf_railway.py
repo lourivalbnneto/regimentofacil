1583cd5d7f40b6ebb8e59648ae3aa89e6c853c8c
lourivalbnneto <lourivalbnneto@gmail.com>
2025-05-09 16:26:53 -0300

    Update vectorize_pdf_railway.py

--- vectorize_pdf_railway.py
+++ vectorize_pdf_railway.py
@@ -165,5 +165,5 @@ async def vetorizar_pdf(item: Item):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
